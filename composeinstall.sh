607cb7f96a7d0af12271afc08b722c5318010959
Fernando García Hansen <fgarcia@humanoide.es>
2022-06-30 16:01:55 +0200

    Update composeinstall.sh

--- composeinstall.sh
+++ composeinstall.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install docker-compose
+sudo apt install curl
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+
