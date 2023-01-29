1b418dde3960ff46b013672e0f2ffa075a529e9d
Fernando García Hansen <fgarcia@humanoide.es>
2023-01-29 10:31:15 +0100

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,1 +1,5 @@
 https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-22-04
+
+[https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-22-04]
+
+
